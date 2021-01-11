5d5776e3d67ca7b541bcabb44995905038152ca1
Cris Angelo Bactad <crisangelobactad@gmail.com>
2021-01-11 09:52:20 +0800

    Particles Added and Breakdown
    Added the ParticleJS and Break down the app into layers for performance.

---
+++ partials/footer.php
@@ -0,0 +1,60 @@
+        <!-- Footer Start -->
+        <div class="footer wow fadeIn" data-wow-delay="0.3s">
+            <div class="container-fluid">
+                <div class="container">
+                    <div class="footer-info">
+                        <h2>CRIS ANGELO BACTAD</h2>
+                        <h3>La Paz, San Narciso, Zambales, Philippines</h3>
+                        <div class="footer-menu">
+                            <p>+63-926-6941-784</p>
+                            <p>crisangelobactad@gmail.com</p>
+                        </div>
+                        <div class="footer-social">
+                            <a href="https://www.facebook.com/froztwind"><i class="fab fa-facebook-f"></i></a>
+                            <a href="angelobactad.github.io"><i class="fab fa-github"></i></a>
+                            <a href=""><i class="fab fa-instagram"></i></a>
+                            <a href="https://www.linkedin.com/froztwind"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="container copyright">
+                    <p>&copy; <a href="#">CRIS B.</a>, All Right Reserved | Designed By <a href="https://facebook.com/froztwind">Cris Angelo Bactad</a></p>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End -->
+        
+        
+        <!-- Back to top button -->
+        <a href="#" class="btn back-to-top"><i class="fa fa-chevron-up"></i></a>
+        
+        
+        <!-- Pre Loader -->
+        <div id="loader" class="show">
+            <div class="loader"></div>
+        </div>
+
+        
+        <!-- JavaScript Libraries -->
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+        <script src="lib/easing/easing.min.js"></script>
+        <script src="lib/wow/wow.min.js"></script>
+        <script src="lib/waypoints/waypoints.min.js"></script>
+        <script src="lib/typed/typed.min.js"></script>
+        <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+        <script src="lib/isotope/isotope.pkgd.min.js"></script>
+        <script src="lib/lightbox/js/lightbox.js"></script>
+
+        <!-- Particlejs File -->
+        <script src="lib/particles/particles.min.js"></script>
+        <script src="lib/particles/particles.js"></script>
+        <script src="lib/particles/app.js"></script> 
+
+        
+        <!-- Contact Javascript File -->
+        <script src="mail/jqBootstrapValidation.min.js"></script>
+        <script src="mail/contact.js"></script>
+
+        <!-- Template Javascript -->
+        <script src="js/main.js"></script>
